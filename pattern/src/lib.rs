2f38a2fb608676dbd10aa69caeacd831ad8e16fb
Maja Kądziołka <maya@compilercrim.es>
2025-02-11 03:34:14 +0100

    Document the pattern language

--- pattern/src/lib.rs
+++ pattern/src/lib.rs
@@ -1,9 +1,107 @@
+//! This crate implements a macro-based DSL for matching netlists.
+//!
+//! The DSL features programmable patterns, which enables many features that
+//! wouldn't otherwise be possible. For example:
+//! - [`PZExt`] can match zero extensions, even though they don't correspond
+//!   to a cell at all, and are achieved by appropriately mixing [`Net`]s
+//!   within a [`Value`]
+//! - patterns which correspond to bitwise cells, such as [`PAnd`], can match
+//!   a [`Value`] even if it is made up from [`Net`]s coming from many distinct
+//!   [`And`] cells.
+//! - specialized patterns can be easily implemented, such as [`PPow2@a`],
+//!   which matches a constant power of 2 and captures the exponent into `a`.
+//!
+//! [`And`]: prjunnamed_netlist::Cell::And
+//! [`Net`]: prjunnamed_netlist::Net
+//! [`Value`]: prjunnamed_netlist::Value
+//! [`PAnd`]: patterns::PAnd
+//! [`PPow2@a`]: patterns::PPow2
+//! [`PZExt`]: patterns::PZExt
+//!
+//! By convention, the names of patterns are prefixed with `P`.
+//!
+//! The general netlist match syntax looks as follows:
+//!
+//! ```no_run
+//! # use prjunnamed_netlist::{Design, Net, Value};
+//! # use prjunnamed_pattern::netlist_matches;
+//! # use prjunnamed_pattern::patterns::*;
+//! let matcher = netlist_matches! {
+//!     [PAdc@y [PAny@a] [PZero] [PInput@c ("cin")]] => (y, a, c);
+//! };
+//! # let design: Design = todo!();
+//! # let target: Value = todo!();
+//! # let result: Option<(Value, Value, Net)> = matcher(&design, &target);
+//! ```
+//!
+//! A pattern call is delimited with square brackets and consists of the
+//! following parts:
+//! - the name of the pattern being called, e.g. `PAdc`
+//! - a result capture, e.g. `@y`. This is optional, and if it is not provided,
+//!   the value captured by the pattern is discarded.
+//!
+//!   Each pattern captures exactly one value. Often, it is the [`Net`] or
+//!   [`Value`] it is being matched against. In rare cases, the captured value
+//!   is of the unit type `()`, in which case it is usually discarded by the
+//!   user.
+//! - a number of expression arguments, e.g. `("cin")`. Expression arguments
+//!   are evaluated according to normal Rust rules, and the results are passed
+//!   to the pattern. Each pattern has a fixed number of expression arguments
+//!   it expects.
+//!
+//!   If multiple expression arguments are being passed to the pattern, each
+//!   requires its own pair of parentheses, e.g. `PFeline@cat ("meow") (42)`
+//! - a number of pattern arguments, e.g. `[PZero]`. Pattern arguments
+//!   follow the pattern call syntax being described.
+//!
+//!   Each pattern has a fixed number of pattern arguments it expects.
+//!   Most commonly, they correspond to the inputs of the cell being matched.
+//!
+//!   If you do not wish to perform further matching on the inputs, use `[PAny]`.
+//!
+//! Much like `match`, [`netlist_matches!`] supports `if` and `if let` guards.
+//! For example:
+//!
+//! ```no_run
+//! # use prjunnamed_netlist::{Design, Net, Value};
+//! # use prjunnamed_pattern::netlist_matches;
+//! # use prjunnamed_pattern::patterns::*;
+//! let matcher = netlist_matches! {
+//!     [PXor [PAny@a] [PAny@b]] if a == b => Value::zero(a.len());
+//! };
+//! # let design: Design = todo!();
+//! # let target: Value = todo!();
+//! # let result: Option<Value> = matcher(&design, &target);
+//! ```
+
+/// A pattern that can be matched against a `Target`.
+///
+/// Most commonly, `Target` will be [`Value`] or [`Net`], but it could also be
+/// something like `u32`.
+///
+/// A pattern must also have a `new` function that will be called by the macro
+/// to construct an instance of the pattern. The arguments of the `new` function
+/// consist of all the expression arguments, followed by all the pattern
+/// arguments.
 pub trait Pattern<Target> {
+    /// The capture type resulting from matching this pattern *and all the
+    /// patterns passed as arguments*.
+    ///
+    /// The macros expect this to be a tuple, where the first element is
+    /// the value captured by the pattern itself, and the following elements
+    /// correspond to the `Capture`s of all the child patterns.
     type Capture;
 
     fn execute(&self, design: &dyn DesignDyn, target: &Target) -> Option<Self::Capture>;
 }
 
+/// Matches a [`Value`] against a list of patterns and runs the body
+/// corresponding to the first matching pattern.
+///
+/// To be specific, `netlist_matches!` returns an
+/// `impl Fn(&dyn DesignDyn, &Value) -> Option<T>`. If no pattern matches
+/// the value provided, the function will return `None`. Otherwise, it will
+/// call `.into()` on the result of the matching arm and return `Some`.
 #[macro_export]
 macro_rules! netlist_match {
     { [ $($rule:tt)* ] $($rest:tt)* } => {
@@ -75,6 +173,11 @@ macro_rules! netlist_match {
     };
 }
 
+/// Like [`netlist_matches!`], but calls
+/// [`replace_value`][Design::replace_value] when a match is found.
+///
+/// The function returned by this macro returns `true` if an applicable rule
+/// has been found.
 #[macro_export]
 macro_rules! netlist_replace {
     { [ $($rule:tt)* ] $($rest:tt)* } => {
--- pattern/src/simple.rs
+++ pattern/src/simple.rs
@@ -2,6 +2,7 @@ use prjunnamed_netlist::{Cell, Const, Net, Trit, Value};
 
 use crate::{DesignDyn, NetOrValue, Pattern};
 
+/// Matches anything. Captures the value being matched.
 pub struct PAny;
 
 impl PAny {
@@ -18,6 +19,10 @@ impl<T: Clone> Pattern<T> for PAny {
     }
 }
 
+/// Captures the value being matched, even if the child pattern captures
+/// something else.
+///
+/// For example, `[PBind@v [PPow2@e]]`.
 pub struct PBind<P>(P);
 
 impl<P> PBind<P> {
@@ -34,6 +39,7 @@ impl<T: Clone, P: Pattern<T>> Pattern<T> for PBind<P> {
     }
 }
 
+/// Matches `Net`s and `Value`s that are constants.
 pub struct PConst;
 
 impl PConst {
@@ -148,6 +154,8 @@ impl<T: NetOrValue> Pattern<T> for PHasX {
     }
 }
 
+/// Matches constants that are powers of two (have exactly one bit set).
+/// Captures the exponent, i.e. log2 of the value.
 pub struct PPow2;
 
 impl PPow2 {
